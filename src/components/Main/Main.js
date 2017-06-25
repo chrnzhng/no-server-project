0ad04626231c03f502360f2748bd6f4420b64c8e
Jordan Zhang <chrn.zhng@gmail.com>
2017-06-25 17:06:18 -0600

    Uploading MVP

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -21,9 +21,9 @@ const Main = (props) => {
         <SearchBar className="search" userInput={props.userInput} handleInputChange={props.userInputFunction} makeRequest={props.onButtonClick}/>
           <ListGroup className="title">
 
-         
-            <ListGroupItem><h4>AQI: {props.resultsObject.breezometer_aqi}</h4></ListGroupItem>     
-            <ListGroupItem><h4>Last Update: { props.resultsObject.datetime }</h4></ListGroupItem>          
+            <ListGroupItem><h4>Last Update: { props.resultsObject.datetime }</h4></ListGroupItem>
+            <ListGroupItem><h4>Country Name: { props.resultsObject.country_name }</h4></ListGroupItem>  
+            <ListGroupItem><h4>AQI: {props.resultsObject.breezometer_aqi}</h4></ListGroupItem>   
             <ListGroupItem><h4>AQI Description: { props.resultsObject.breezometer_description }</h4></ListGroupItem>
             <ListGroupItem><h4>Dominant Pollutant: { props.resultsObject.dominant_pollutant_canonical_name }</h4></ListGroupItem>
             {/*<ListGroupItem className=""><h4>Main: { props.resultsObject.dominant_pollutant_text}</h4></ListGroupItem>*/}
@@ -35,4 +35,5 @@ const Main = (props) => {
 };
 //breezometer_aqi,random_recommendations,datetime,pollutants,breezometer_description
 export default Main;
+                    
 
