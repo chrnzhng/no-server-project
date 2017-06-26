d4624cf5ce465ae5d765492c1ef4a0176085f013
Jordan Zhang <chrn.zhng@gmail.com>
2017-06-25 18:07:02 -0600

    Added some final touches

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,8 @@ import React from 'react';
 const Footer = () => {
     return (
         <div className="footerBar">                
-      <a href="http://blog.breezometer.com/hubfs/BAQI%20Scale%20-%20BreezoMeter%20Global%20Air%20Quality%20Index.png?t=1498228196392" target="blank"><h5 className="bottom-text">The BAQI Scale</h5></a>
+      <a href="http://ip-api.com/docs/api:json" target="blank">
+      <h5 className="bottom-text">Geolocation API</h5></a>
       {/*<a><img src={global} className="bottom-image" alt="globe"/></a>*/}
       
     </div>
@@ -14,3 +15,6 @@ const Footer = () => {
 };
 
 export default Footer;
+
+//https://en.wikipedia.org/wiki/Air_pollution#Pollutants
+//http://blog.breezometer.com/hubfs/BAQI%20Scale%20-%20BreezoMeter%20Global%20Air%20Quality%20Index.png?t=1498228196392
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -21,11 +21,11 @@ const Main = (props) => {
         <SearchBar className="search" userInput={props.userInput} handleInputChange={props.userInputFunction} makeRequest={props.onButtonClick}/>
           <ListGroup className="title">
 
-            <ListGroupItem><h4>Last Update: { props.resultsObject.datetime }</h4></ListGroupItem>
-            <ListGroupItem><h4>Country Name: { props.resultsObject.country_name }</h4></ListGroupItem>  
-            <ListGroupItem><h4>AQI: {props.resultsObject.breezometer_aqi}</h4></ListGroupItem>   
-            <ListGroupItem><h4>AQI Description: { props.resultsObject.breezometer_description }</h4></ListGroupItem>
-            <ListGroupItem><h4>Dominant Pollutant: { props.resultsObject.dominant_pollutant_canonical_name }</h4></ListGroupItem>
+            <ListGroupItem><h5>Last Update: { props.resultsObject.datetime }</h5></ListGroupItem>
+            <ListGroupItem><h5>Country Name: { props.resultsObject.country_name }</h5></ListGroupItem>  
+            <ListGroupItem><a href="http://blog.breezometer.com/hubfs/BAQI%20Scale%20-%20BreezoMeter%20Global%20Air%20Quality%20Index.png?t=1498228196392" target="blank"><h5>AQI: {props.resultsObject.breezometer_aqi}</h5></a></ListGroupItem>   
+            <ListGroupItem><h5>AQI Description: { props.resultsObject.breezometer_description }</h5></ListGroupItem>
+            <ListGroupItem><a href="https://en.wikipedia.org/wiki/Air_pollution#Pollutants" target="blank"><h5>Dominant Pollutant: { props.resultsObject.dominant_pollutant_canonical_name }</h5></a></ListGroupItem>
             {/*<ListGroupItem className=""><h4>Main: { props.resultsObject.dominant_pollutant_text}</h4></ListGroupItem>*/}
             {/*<ListGroupItem className=""><h4>Effects: { props.resultsObject.dominant_pollutant_text[effects] }</h4></ListGroupItem>
             <ListGroupItem className=""><h4>Causes: { props.resultsObject.dominant_pollutant_text[causes] }</h4></ListGroupItem>*/}
